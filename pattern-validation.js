e9f3d4b78366af6294085c27efcfbbe80f748160
Manish K <manish2012.2016@gmail.com>
2024-12-12 23:41:12 +0530

    2nd commit

--- pattern-validation.js
+++ pattern-validation.js
@@ -1,6 +1,40 @@
-function patternValidate(){
-
+function patternValidate(pattern) {
+  let stack = [];
+  for (let index = 0; index < pattern.length; index++) {
+    const currentString = pattern[index];
+    if (
+      currentString === "(" ||
+      currentString === "{" ||
+      currentString === "["
+    ) {
+      stack.push(currentString);
+      continue;
+    }
+    if (!stack.length) return false;
+    let popString = stack.pop();
+    switch (currentString) {
+      case ")":
+        if (popString === "{" || popString === "[") return false;
+        break;
+      case "}":
+        if (popString === "(" || popString === "[") return false;
+        break;
+      case "[":
+        if (popString === "(" || popString === "{") return false;
+        break;
+    }
+  }
+  return !stack.length;
 }
 
-patternValidate('{[](]'); //invalid pattern
-patternValidate('{[]}()[]{[{}]}'); //valid pattern
+function validateThePattern(pattern) {
+  if (patternValidate(pattern)) {
+    console.log("Valid pattern");
+  } else {
+    console.log("Invalid pattern");
+  }
+}
+
+validateThePattern("{[](]"); //invalid pattern
+validateThePattern("{[()]}"); //valid pattern
+validateThePattern("{[]}()[]{[{}]}"); //valid pattern
